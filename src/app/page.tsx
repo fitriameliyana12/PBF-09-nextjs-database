e6e05aac9b1011d989d8dc689618153266e21d4e
fitriameliyana12 <fitriameliyana1201@gmail.com>
2024-05-14 22:31:07 +0700

    hm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,63 @@
-import { Card } from '@/app/components/molecules/card';
-import RevenueChart from '@/app/components/molecules/revenue-chart';
-import LatestInvoices from '@/app/components/molecules/latest-invoices';
+import { generateYAxis } from '@/utils/utils';
+import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/components/atoms/fonts';
+import { fetchRevenue } from '@/model/query';
 
-export default async function Page() {
-  // Dummy data for demonstration purposes
-  // const totalPaidInvoices = 1000;
-  // const totalPendingInvoices = 500;
-  // const numberOfInvoices = 1500;
-  // const numberOfCustomers = 200;
+// This component is representational only.
+// For data visualization UI, check out:
+// https://www.tremor.so/
+// https://www.chartjs.org/
+// https://airbnb.io/visx/
 
-  return (
-    <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
-      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
-      </div> */}
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
-        <div className="md:col-span-1">
-          <RevenueChart />
+export default async function RevenueChart() {
+    const revenue = await fetchRevenue();
+
+    const chartHeight = 350;
+    const { yAxisLabels, topLabel } = generateYAxis(revenue);
+
+    if (!revenue || revenue.length === 0) {
+        return <p className="mt-4 text-gray-400">No data available.</p>;
+    }
+
+    return (
+        <div className="w-full">
+            <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Recent Revenue
+            </h2>
+            <div className="rounded-xl bg-gray-50 p-4">
+                <div className="mt-0 flex items-end gap-4 rounded-md bg-white p-4" style={{ width: '100%' }}>
+                    {/* y-axis */}
+                    <div
+                        className="mb-6 flex flex-col justify-between text-sm text-gray-400"
+                        style={{ height: `${chartHeight}px` }}
+                    >
+                        {yAxisLabels.map((label) => (
+                            <p key={label}>{label}</p>
+                        ))}
+                    </div>
+
+                    {revenue.map((month) => (
+                        <div key={month.month} className="flex flex-col items-center gap-2">
+                            {/* bars */}
+                            <div
+                                className="w-full rounded-md bg-blue-300"
+                                style={{
+                                    height: `${(chartHeight / topLabel) * month.revenue}px`,
+                                    width: '40px', // Adjust the width of each bar
+                                }}
+                            ></div>
+                            {/* x-axis */}
+                            <p className="text-sm text-gray-400">
+                                {month.month}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex items-center pb-2 pt-6">
+                    <CalendarIcon className="h-5 w-5 text-gray-500" />
+                    <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+                </div>
+            </div>
         </div>
-        <div className="md:col-span-1">
-          <LatestInvoices />
-        </div>
-      </div>
-    </main>
-  );
+    );
 }
