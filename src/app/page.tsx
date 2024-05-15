9669dd2db3dc4c4ed2f3303b3409886397d5d5ca
fitriameliyana12 <fitriameliyana1201@gmail.com>
2024-05-15 12:34:39 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,28 @@ import { lusitana } from '@/app/components/atoms/fonts';
 
 export default async function Page() {
   // Dummy data for demonstration purposes
-  // const totalPaidInvoices = 1000;
-  // const totalPendingInvoices = 500;
-  // const numberOfInvoices = 1500;
-  // const numberOfCustomers = 200;
+  const totalPaidInvoices = 1000;
+  const totalPendingInvoices = 500;
+  const numberOfInvoices = 1500;
+  const numberOfCustomers = 200;
 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card title="Total Customers" value={numberOfCustomers} type="customers" />
-      </div> */}
+      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
         <div className="md:col-span-1 flex flex-col">
           <RevenueChart />
         </div>
         <div className="md:col-span-1 flex flex-col">
-          {/* <LatestInvoices /> */}
+          <LatestInvoices />
         </div>
       </div>
     </main>
